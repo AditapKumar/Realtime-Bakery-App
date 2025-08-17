c798d3ce9eb1412dfe2de0b9e2b9a115958a1947
AditapKumar <aditaprawal2004@gmail.com>
2025-08-17 22:39:39 +0530

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+// Import your CSS
+import "../css/app.css";
+
+// Your JavaScript code can go here
+console.log("Bakery app loaded!");
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./resources/**/*.{html,js,ejs}",
+      "./resources/views/**/*.ejs",
+      "./public/**/*.html"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
